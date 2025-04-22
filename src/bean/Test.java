763fee56d96dd3d3b5b9866758b288a32c67ab73
h_airu <h_airu@LAPTOP-O1PH9M7L>
2025-04-22 11:51:46 +0900

    123

--- src/bean/Test.java
+++ src/bean/Test.java
@@ -5,7 +5,7 @@ import java.io.Serializable;
 public class Test extends User implements Serializable{
 	private String student_no;
 	private String subject_no;
-	private String wchool_cd;
+	private String school_cd;
 	private int no;
 	private int point;
 	private String class_num;
@@ -14,7 +14,7 @@ public class Test extends User implements Serializable{
         return student_no;
     }
 
-    public void SetstudentNo(String student_no) {
+    public void setStudentNo(String student_no) {
         this.student_no = student_no;
     }
 
@@ -26,5 +26,12 @@ public class Test extends User implements Serializable{
         this.subject_no = subject_no;
     }
 
+    public String getSchoolCd() {
+        return school_cd;
+    }
+
+    public void setSchoolCd(String school_cd) {
+        this.school_cd = school_cd;
+    }
 
 }
