fefb5eb8e3ffa0a82b62024c7202138e8392e04d
admin <kmm2459303@stu.o-hara.ac.jp>
2025-04-23 11:39:28 +0900

    aa

--- src/scoremanager/main/LoginAction.java
+++ src/scoremanager/main/LoginAction.java
@@ -4,9 +4,7 @@ import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
-import bean.School;
 import bean.Teacher;
-import dao.SchoolDao;
 import dao.TeacherDao;
 import tool.Action;
 
@@ -21,10 +19,6 @@ public class LoginAction extends Action {
 
             TeacherDao dao = new TeacherDao();
             Teacher teacher = dao.search(id, password); // ← ここでDB接続エラーが起こる可能性あり
-
-            SchoolDao sdao=new SchoolDao();
-            School school=sdao.get(teacher.getSchool_cd());
-            teacher.setSchool(school);
 
             if (teacher != null) {
                 session.setAttribute("teacher", teacher);
