ca5558cb54de433f7d94a02364bf1cb05cf6815f
kmm2459342 <kmm2459342@stu.o-hara.ac.jp>
2025-04-30 10:05:15 +0900

    Update TestListStudentDao.java

--- src/dao/TestListStudentDao.java
+++ src/dao/TestListStudentDao.java
@@ -3,7 +3,6 @@ package dao;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -11,34 +10,29 @@ import bean.Student;
 import bean.TestListStudent;
 
 public class TestListStudentDao extends Dao {
+
     private String baseSql =
-        "SELECT subj.NAME AS subject_name, subj.CD AS subject_cd, t.NO AS num, t.POINT AS point " +
+        "SELECT t.NO AS TEST_NO, t.POINT, sub.NAME AS SUBJECT_NAME, sub.CD AS SUBJECT_CD " +
         "FROM TEST t " +
-        "JOIN SUBJECT subj ON t.SUBJECT_CD = subj.CD AND t.SCHOOL_CD = subj.SCHOOL_CD " +
-        "WHERE t.STUDENT_NO = ? " +
-        "AND t.SCHOOL_CD = ? " +
-        "AND t.CLASS_NUM = ? " +
-        "AND EXISTS ( " +
-        "    SELECT 1 FROM STUDENT s " +
-        "    WHERE s.NO = t.STUDENT_NO " +
-        "    AND s.ENT_YEAR = ? " +
-        ")";
+        "JOIN SUBJECT sub ON t.SUBJECT_CD = sub.CD AND t.SCHOOL_CD = sub.SCHOOL_CD " +
+        "WHERE t.STUDENT_NO = ? AND t.SCHOOL_CD = ? " +
+        "ORDER BY t.SUBJECT_CD, t.NO";
 
     public List<TestListStudent> postFilter(ResultSet rSet) {
-        List<TestListStudent> result = new ArrayList<>();
+        List<TestListStudent> list = new ArrayList<>();
         try {
             while (rSet.next()) {
-                TestListStudent student = new TestListStudent();
-                student.setSubjectName(rSet.getString("subject_name"));
-                student.setSubjectCd(rSet.getString("subject_cd"));
-                student.setNum(rSet.getInt("num"));
-                student.setPoint(rSet.getInt("point"));
-                result.add(student);
+                TestListStudent bean = new TestListStudent();
+                bean.setSubjectName(rSet.getString("SUBJECT_NAME"));
+                bean.setSubjectCd(rSet.getString("SUBJECT_CD"));
+                bean.setNum(rSet.getInt("TEST_NO"));
+                bean.setPoint(rSet.getInt("POINT"));
+                list.add(bean);
             }
-        } catch (SQLException e) {
+        } catch (Exception e) {
             e.printStackTrace();
         }
-        return result;
+        return list;
     }
 
     public List<TestListStudent> filter(Student student) {
@@ -49,8 +43,6 @@ public class TestListStudentDao extends Dao {
         ) {
             st.setString(1, student.getNo());
             st.setString(2, student.getSchool().getCd());
-            st.setString(3, student.getClassNum());
-            st.setInt(4, student.getEntYear());
 
             try (ResultSet rs = st.executeQuery()) {
                 result = postFilter(rs);
@@ -60,5 +52,4 @@ public class TestListStudentDao extends Dao {
         }
         return result;
     }
-
 }
